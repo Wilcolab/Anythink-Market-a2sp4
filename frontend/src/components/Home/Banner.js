0e6ac076973bb24a59d43eedb31239e898f26a57
Dar Malovani <dar.malovani@wilcohq.com>
2022-09-22 11:41:52 +0300

    feat: hide search boxk

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import agent from "../../agent";
 
 const Banner = (props) => {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <input
-            type="input"
-            id="search-box"
-            onChange={({ target }) => {
-              const title = target.value;
-              if (title.length > 2) {
-                props.onClickSearch(
-                  title,
-                  (page) => agent.Items.byTitle(title, page),
-                  agent.Items.byTitle(title)
-                );
-              }
-            }}
-          />
+          <span id="get-part" onClick={() => setShowSearch(true)}>
+            A place to get
+          </span>
+          {showSearch && (
+            <input
+              type="input"
+              id="search-box"
+              onChange={({ target }) => {
+                const title = target.value;
+                if (title.length > 2) {
+                  props.onClickSearch(
+                    title,
+                    (page) => agent.Items.byTitle(title, page),
+                    agent.Items.byTitle(title)
+                  );
+                }
+              }}
+            />
+          )}
           <span> the cool stuff.</span>
         </div>
       </div>
